b56fbea88772a370d7608a8749d201606136256a
Renato Dias <diasrenato@live.com>
2021-08-09 22:36:53 -0300

    fazendo as primeiras partes do site do flavin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,20 @@
+import { Route } from '@angular/compiler/src/core';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    }
+
+]
+
+@NgModule({
+    imports: [ 
+        RouterModule.forRoot(routes) 
+    ],
+    exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
